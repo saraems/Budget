cfaf96b92732ebd1e567640eca01fbee6ee9e55b
saraems <sara.ems.94@gmail.com>
2019-04-21 21:12:31 +0200

    Updates

---
+++ precache-manifest.bd9c7c3b13f1acb9afb19b5cd4357b48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8ee21c05fe4b357383258254345fe9a",
+    "url": "/Budget/static/media/Your_budget_logo.d8ee21c0.svg"
+  },
+  {
+    "revision": "3310ab6d9d3be2cc98191aba785c40cd",
+    "url": "/Budget/static/media/Smartphone_budget_logo.3310ab6d.png"
+  },
+  {
+    "revision": "ae11d4515cb0017687ca",
+    "url": "/Budget/static/js/runtime~main.f43c1cf9.js"
+  },
+  {
+    "revision": "316e2f1e89107fa9a67a",
+    "url": "/Budget/static/js/main.f3ad370f.chunk.js"
+  },
+  {
+    "revision": "0cfbf09e5b65deaba595",
+    "url": "/Budget/static/js/2.2f8c987b.chunk.js"
+  },
+  {
+    "revision": "316e2f1e89107fa9a67a",
+    "url": "/Budget/static/css/main.f3f2f140.chunk.css"
+  },
+  {
+    "revision": "433e020c1a542fc2992917dc5efb1062",
+    "url": "/Budget/index.html"
+  }
+];
