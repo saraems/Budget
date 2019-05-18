fac9e49255d8c198ce77f672ca47c29334fccc0f
saraems <sara.ems.94@gmail.com>
2019-05-18 14:45:38 +0200

    user-panel page

--- src/components/user-panel/UserPanel.js
+++ src/components/user-panel/UserPanel.js
@@ -8,10 +8,10 @@ class UserPanel extends Component {
     return (
         <Fragment>
             <header className='user_panel__header'>
-                <div className="page_container">
+                <div className="page_container user_panel__header_container">
+                    <div className='user_panel_nav__logo'> Twoje Finanse </div>
                     <ul className="user_panel__nav">
-                        <div className='user_panel_nav__logo'> Twoje Finanse </div>
-                        <li>Poprzedni miesiac</li>
+                        <li>Dodaj</li>
                         <li>Wydatki</li>
                         <li>Porownaj</li>
                         <li>Oszczednosci</li>
@@ -23,12 +23,13 @@ class UserPanel extends Component {
                 <div className="user_panel___main_panel page_container">
                     <div className="row">
 
-                        <section className="col-12-12 user_panel__add_current_month">
-                            <h1> Dodaj podsumowanie z poprzedniego miesiaca</h1>
-                            <h2>Wybierz plik <span className='file_format'>.csv</span> lub <span className='file_format'>.xml</span>. Nie wiesz gdzie je znalezc? Pobierz je ze swojego konta bankowego on-line.</h2>
-                            <a className='select_file_btn'>Wybierz</a>
-                            <p>Jesli nie masz konta w banku, lub nie mozesz uzyskac podsumowania miesiaca w podamym formacie. Nic straconego! Dodaj swoje wydatki za pomoca formularza.</p>
-                            <a className="add_monthly_expenses">Dodaj</a>
+                        <section className="col-12-12 user_panel__add_document">
+                            <div className='user_panel__add_document__header'> Dodaj podsumowanie</div>
+                            <div className='user_panel__add_document__header_subheader'>Wybierz plik <span className='highlighted'>.csv</span> lub <span className='highlighted'>.xml</span>.</div>
+                            <div className='user_panel__add_document__tip'> Nie wiesz gdzie je znalezc? Pobierz je ze swojego konta bankowego on-line.</div>
+                            <a className='uploader__btn action__btn'>Wybierz dokument</a>
+                            <p className='user_panel__add_document__form'>Jesli nie masz konta w banku, lub nie mozesz uzyskac podsumowania miesiaca w podamym formacie. <br/>Dodaj swoje wydatki za pomoca formularza:</p>
+                            <a className="add_form_btn action__btn">Wypelnij formularz</a>
                         </section>
                         <section className="user_panel__add_previous_months">
 
