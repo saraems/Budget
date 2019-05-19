49c7b9ebbcece9f326bc669537770cc87c6907da
saraems <sara.ems.94@gmail.com>
2019-05-19 22:42:34 +0200

    rechaart library added to the project

--- src/components/user-panel/Expenses/Expenses.js
+++ src/components/user-panel/Expenses/Expenses.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import './Expenses.scss';
+
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 400, pv: 2400, amt: 2400}, {name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
 
 
 class Expenses extends Component {
 
-
     render() {
         return (
-            <div>
-                Expenses !
-            </div>
+            <LineChart width={600} height={300} data={data}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            </LineChart>
         );
     }
 }
